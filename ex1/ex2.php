f405b194e61dbe3f795e78a4474b8fdf4c252252
paveka <paveka@taltech.ee>
2025-09-05 12:07:28 +0300

    ex1

--- ex1/ex2.php
+++ ex1/ex2.php
@@ -2,13 +2,13 @@
 
 $list = [1, 2, 3, 2, 6];
 
-var_dump(isInList($list, 3));
+var_dump(isInList($list, '3'));
 
 var_dump(isInList([1, 2, 3], 4));
 
 function isInList($list, $target): bool {
     // kood tuleb siia
-    return in_array($target, $list);
+    return in_array($target, $list, true);
 }
 
 
