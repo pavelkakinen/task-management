a32def6ec43975ad4b68500f71a7ac1cd853486e
paveka <paveka@taltech.ee>
2025-09-12 20:05:32 +0300

    ex2 done

--- ex2/functions.php
+++ ex2/functions.php
@@ -2,44 +2,35 @@
 
 function getAverageWinterTemp(int $winterStartYear, int $winterEndYear): float {
 
-    $file = fopen(__DIR__ . "/../ex1/temperatures-filtered.csv", "r");
-    $totalTemp = 0;
-    $count = 0;
+    $file = fopen(__DIR__ . "/../ex1/data/temperatures-filtered.csv", "r");
+    $allTemp = [];
 
-    // Winter months: December of start year, January and February of end year
-    $winterMonths = [
-        [$winterStartYear, 12],  // December of start year
-        [$winterEndYear, 1],     // January of end year
-        [$winterEndYear, 2]      // February of end year
-    ];
+    if (!$file) {
+        return 0.0;
+    }
 
     while (!feof($file)) {
-        $data = fgetcsv($file);
+        $dict = fgetcsv($file);
 
-        if (empty($data)) {
+        if (empty($dict)) {
             continue;
         }
 
-        $year = intval($data[0]);
-        $month = intval($data[1]);
-        $temperature = floatval($data[4]);
-
-        // Check if this record is part of the winter period
-        foreach ($winterMonths as $winterMonth) {
-            if ($year === $winterMonth[0] && $month === $winterMonth[1]) {
-                $totalTemp += $temperature;
-                $count++;
-                break;
-            }
+        $year = intval($dict[0]);
+        $month = intval($dict[1]);
+        $temperature = floatval($dict[4]);
+        if ($year === $winterStartYear && $month === 12) {
+            $allTemp[] = $temperature;
+        } elseif ($year === $winterEndYear && ($month === 1 || $month === 2)) {
+            $allTemp[] = $temperature;
         }
     }
 
     fclose($file);
-
-    if ($count === 0) {
-        return 0.0;
+    if (empty($allTemp)) {
+        return 0;
     }
+    $result = (array_sum($allTemp) / count($allTemp));
 
-    return round($totalTemp / $count, 2);
+    return round($result, 2);
 }
-
